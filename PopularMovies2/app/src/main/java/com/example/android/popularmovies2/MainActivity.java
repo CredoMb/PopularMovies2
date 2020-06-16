a4af91794ed8fa2b905945b959523ceb10cae31d
Credo-Mbikayi <credo.mbikayi@umontreal.ca>
2020-06-15 20:21:06 -0400

    Add the primary variables to the MainActivity class

--- PopularMovies2/app/src/main/java/com/example/android/popularmovies2/MainActivity.java
+++ PopularMovies2/app/src/main/java/com/example/android/popularmovies2/MainActivity.java
@@ -1,15 +1,96 @@
 package com.example.android.popularmovies2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+
+import com.example.android.popularmovies2.Data.AMovie;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * The Ids for the 2 Loaders to be used in
+     * this activity.
+     */
+    private static final int MOVIE_LOADER_ID = 0;
+
+    /**
+     * Will be used as values for the mSort variable.
+     * This will determine the order with which the movies
+     * are displayed inside the Main Activity
+     */
+    private String BY_POPULARITY = "popularity.desc";
+    private String BY_RATINGS = "vote_average.desc";
+
+    /**
+     * Will be used as the base url and parameter will be
+     */
+    private String MOVIE_REQUEST_URL = "https://api.themoviedb.org/3/discover/movie?";
+
+    /**
+     * The key used to save the movie list as
+     * a bundle.
+     */
+    public String MOVIE_LIST = "movie_list";
+
+    /**
+     * The Recycler and its Adapter
+     */
+    private RecyclerView mRecyclerView;
+    private MovieAdapter mMovieAdapter;
+
+    /**
+     * The variable that should contain the movie list
+     */
+
+    public static ArrayList<AMovie> mMovieList;
+
+    /**
+     * The progress Spinner
+     */
+    private ProgressBar mProgressSpinner;
+
+    /**
+     * Will store the sorting option.
+     * The popularity is the default
+     * way of displaying the movies in the main
+     * Activity.
+     */
+    private String mSortBy;
+
+    /**
+     * The group view that will contain the
+     * empty state for a bad internet connection
+     */
+
+    private RelativeLayout emptyStateRl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize the movie list to make sure there
+        // are no "null pointer" exceptions
+        mMovieList = new ArrayList<AMovie>();
+
+        // Verify if the movie list was saved as a bundle
+        // and retrieve the data.
+        if (savedInstanceState != null) {
+
+            if (savedInstanceState.containsKey(MOVIE_LIST)) {
+               // mMovieList = savedInstanceState.getParcelableArrayList(MOVIE_LIST);
+
+            }
+        }
+
+
+
     }
 }
