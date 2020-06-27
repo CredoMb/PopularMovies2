912474d59bc7f9f86d1528a2f017dc1e2d2023f8
Credo-Mbikayi <credo.mbikayi@umontreal.ca>
2020-06-26 23:17:55 -0400

    Build the APIClient class. This will contain the base url for to access the API

---
+++ PopularMovies2/app/src/main/java/com/example/android/popularmovies2/Data/APIClient.java
@@ -0,0 +1,26 @@
+package com.example.android.popularmovies2.Data;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class APIClient {
+
+    private static Retrofit retrofit = null;
+    public final String BASE_URL = "https://api.themoviedb.org/3/";
+
+    static Retrofit getClient() {
+
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl("https://api.themoviedb.org/3/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build();
+
+        return retrofit;
+    }
+}
